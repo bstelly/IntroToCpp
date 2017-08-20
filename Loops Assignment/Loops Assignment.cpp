f40df529859dd5ded91db618d91ee81e587ba998
bstelly <bstelly1999@gmail.com>
2017-08-20 12:15:20 -0500

    Update Loops Assignment.cpp

--- Loops Assignment/Loops Assignment.cpp
+++ Loops Assignment/Loops Assignment.cpp
@@ -8,14 +8,14 @@ int main()
 	{
 		std::cout << i << std::endl;
 	}
-	//
+	//The output will be 0 and continue counting up to 99.
 
 	//b. 
 	for (int i = 100; i >= 0; i -= 2)
 	{
 		std::cout << i << std::endl;
 	}
-	//c. 
+	//c.The ouput will be 100 and count down by 2 until it reaches 0.
 	int iter = 0;
 	while (iter != 25)
 	{
@@ -34,9 +34,9 @@ int main()
 	} while (counter < 10);
 
 	//2. Using a for loop, iterate through numbers 0 to 100.
-	//-For multiples of 3 print “Fizz”
-	//- For multiples of 5 print “Buzz”
-	//- For multiples of 3 and 5 print “FizzBuzz”
+	//-For multiples of 3 print â€œFizzâ€
+	//- For multiples of 5 print â€œBuzzâ€
+	//- For multiples of 3 and 5 print â€œFizzBuzzâ€
 	//- Otherwise print the number.
 	//Note, you will need to use the modulus operator (%)
 
