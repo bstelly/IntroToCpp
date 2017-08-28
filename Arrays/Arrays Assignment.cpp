55777dbedd7cc48832a539ab79588bb1fbaecbb5
bstelly <bstelly1999@gmail.com>
2017-08-28 05:58:18 -0500

    Arrays Assignment

--- Arrays/Arrays Assignment.cpp
+++ Arrays/Arrays Assignment.cpp
@@ -50,7 +50,7 @@ int main()
 	
 	
 	//5. Using a loop, populate an array of size 10 with the values 10 - 1.
-	int i;
+	
 	int numbers[10];
 
 	for (int i = 10; i > 0 ; i -= 1);
@@ -64,26 +64,62 @@ int main()
 	//6. Create an array of size five then ask the user to input five 
 	//number into the array. Display the largest and smallest numbers 
 	//to the console.
+	
 	int anArray[5];
-	std::cin >> anArray[0];
-	std::cin >> anArray[1];
-	std::cin >> anArray[2];
-	std::cin >> anArray[3];
-	std::cin >> anArray[4];
-	
-	
+	int minNum = anArray[0];
+	int maxNum = anArray[0];
+	for (int num = 0; num < 4; num++)
+	{
+		std::cout << "Input  five numbers.";
+		std::cin >> anArray[num];
+	}
+	for (int iter = 0; iter < 4; iter++)
+	{
+		if (iter < minNum)
+		{
+			minNum = iter;
+			std::cout << "The smallest number is " << minNum << std::endl;
+		}
+		for (int iter = 0; iter < 4; iter++)
+		{
+			if (iter > maxNum)
+			{
+				minNum = iter;
+				std::cout << "The largest number is " << maxNum << std::endl;
+			}
+		}
 
 	
 	
-
-
+		system("pause");
+	
+	
 	//7. Create a program that creates a small 3x3 array of integers. Use a nested for 
-	//loop to initialise the numbers 1 - 9. Output the numbers in a grid format.
-	int grid[3][3];
-	
-	
-
-
+		//loop to initialise the numbers 1 - 9. Output the numbers in a grid format.
+		int grid[3][3];
+		int row;
+		int col;
+		for (row = 0; row < 1; row++)
+		{
+			for (col = 0; col < 3; col++)
+			{
+				std::cout << grid[row][col];
+			}
+		}
+		for (row = 1; row < 2; row++)
+		{
+			for (col = 0; col < 3; col++)
+			{
+				std::cout << grid[row][col];
+			}
+		}
+		for (row = 2; row < 3; row++)
+		{
+			for (col = 0; col < 3; col++)
+			{
+				std::cout << grid[row][col];
+			}
+		}
 
 
 
