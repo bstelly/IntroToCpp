073988e6de0092d5c8aea242acbc7681f1345601
bstelly <bstelly1999@gmail.com>
2017-09-02 21:56:22 -0500

    Practice

---
+++ Practice/Source.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main()
+{
+
+
+
+	//Practice comparing data in an array and printing smallest and largest values
+
+
+
+	int myArray[5];
+	for (int iter = 0; iter < 5; iter++);
+
+
+
+
+
+
+
+
+
+
+
+
+}
