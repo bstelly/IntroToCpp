434b2838a244927f40b1a907f1167786f3b08612
bstelly <bstelly1999@gmail.com>
2017-09-06 12:50:25 -0500

    TicTacToe Project

---
+++ TicTacToe/Source.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+	
+void Grid()
+{
+	int grid[3][3];
+	std::cout << grid[0][0] << "|" << grid[0][1] << "|" << grid[0][2] << std::endl;
+	std::cout << "=====" << std::endl;
+	std::cout << grid[1][0] << "|" << grid[1][1] << "|" << grid[1][2] << std::endl;
+	std::cout << "=====" << std::endl;
+	std::cout << grid[2][0] << "|" << grid[2][1] << "|" << grid[2][2] << std::endl;
+}
+
+
+
+
+int main()
+{
+	
+	Grid();
+
+
+
+
+
+
+
+
+
+	system("pause");
+
+
+}
+
+
+
+
