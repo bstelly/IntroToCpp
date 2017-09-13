5a1f269c030d7f5e23285a2e80bef06729c6141f
bstelly <bstelly1999@gmail.com>
2017-09-13 12:24:39 -0500

    Zombie Fight Club Project

---
+++ Zombie Fight Club/Source.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+//Create a class representing a zombie.
+//- Each zombie should have health and an attack value.
+//- Ensure that the zombie's variables are private and you have functions that allow us to modify the zombie data.
+//- Using the zombie class, create a game where a number of zombies attack each other until only one remains.
+//- You must have a written UML of the zombie class.This can be done either on paper, google draw, gliffy.
+
