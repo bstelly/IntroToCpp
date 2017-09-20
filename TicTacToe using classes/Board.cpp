4aee632ceb8014391499dc72cc64a390b632f8e7
bstelly <bstelly1999@gmail.com>
2017-09-20 12:51:16 -0500

    TicTacToe using classes

---
+++ TicTacToe using classes/Board.cpp
@@ -0,0 +1,6 @@
+#include "Board.h"
+
+Board::Board()
+{
+	char mboard[9] = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+}
--- TicTacToe using classes/Board.h
+++ TicTacToe using classes/Board.h
@@ -1,1 +1,9 @@
 #pragma once
+
+class Board
+{
+public:
+	Board();
+	void PlacePiece(char piece);
+	
+};
