da0324b8b21e7177926a7325f0a0313065f6089a
bstelly <bstelly1999@gmail.com>
2017-09-20 09:07:23 -0500

    Added function to replay
    still needs function to clear board.

--- TicTacToe/Source.cpp
+++ TicTacToe/Source.cpp
@@ -59,7 +59,8 @@ int GameOver()
 	}
 }
 
-int main()
+
+void PlayGame()
 {
 	std::cout << "Tic Tac Toe" << std::endl;
 	std::cout << " " << std::endl;
@@ -67,67 +68,67 @@ int main()
 	std::cout << "Player One is 'X' and Player Two is 'O'.  On your turn, choose a number to place your 'X' or 'O'." << std::endl;
 	std::cout << " " << std::endl;
 	//Board is generated and displayed
-	
+
 	int player = 1;
 	int input;
 	int game;     //  1 - game is being played,   2 - game over with winner,     0 - game over with no winner
 
-	//main game loop
+				  //main game loop
 	do
-		{	
-			Board();
-			player = (player % 2) ? 1 : 2;
-			std::cout << "Player " << player << " it is your turn. Enter a number." << std::endl;
-			std::cin >> input;
-			piece = (player == 1) ? 'X' : 'O';		//Ternary operator to determine which piece is used for which player
-			system("cls");
-			if (input == 1 && board[0] == '1')			
-			{
-				board[0] = piece;
-			}
-			else if (input == 2 && board[1] == '2')
-			{
-				board[1] = piece;
-			}
-			else if (input == 3 && board[2] == '3')
-			{
-				board[2] = piece;
-			}
-			else if (input == 4 && board[3] == '4')
-			{
-				board[3] = piece;
-			}
-			else if (input == 5 && board[4] == '5')
-			{
-				board[4] = piece;
-			}
-			else if (input == 6 && board[5] == '6')
-			{
-				board[5] = piece;
-			}
-			else if (input == 7 && board[6] == '7')
-			{
-				board[6] = piece;
-			}
-			else if (input == 8 && board[7] == '8')
-			{
-				board[7] = piece;
-			}
-			else if (input == 9 && board[8] == '9')
-			{
-				board[8] = piece;
-			}
-			else
-			{
-				std::cout << "Error. Invalid Move" << std::endl;
-				player--;     //Allows Player 2 to try again
-			}
-			game = GameOver();
-			player++;
+	{
+		Board();
+		player = (player % 2) ? 1 : 2;
+		std::cout << "Player " << player << " it is your turn. Enter a number." << std::endl;
+		std::cin >> input;
+		piece = (player == 1) ? 'X' : 'O';		//Ternary operator to determine which piece is used for which player
+		system("cls");
+		if (input == 1 && board[0] == '1')
+		{
+			board[0] = piece;
+		}
+		else if (input == 2 && board[1] == '2')
+		{
+			board[1] = piece;
+		}
+		else if (input == 3 && board[2] == '3')
+		{
+			board[2] = piece;
+		}
+		else if (input == 4 && board[3] == '4')
+		{
+			board[3] = piece;
+		}
+		else if (input == 5 && board[4] == '5')
+		{
+			board[4] = piece;
+		}
+		else if (input == 6 && board[5] == '6')
+		{
+			board[5] = piece;
+		}
+		else if (input == 7 && board[6] == '7')
+		{
+			board[6] = piece;
+		}
+		else if (input == 8 && board[7] == '8')
+		{
+			board[7] = piece;
+		}
+		else if (input == 9 && board[8] == '9')
+		{
+			board[8] = piece;
+		}
+		else
+		{
+			std::cout << "Error. Invalid Move" << std::endl;
+			player--;     //Allows Player 2 to try again
+		}
+		game = GameOver();
+		player++;
 	} while (game == 1);
-	
+
 	Board();
-	
+
 	if (game == 2)
 	{
 		player--;
@@ -139,6 +140,29 @@ int main()
 		std::cout << "Draw. Game Over." << std::endl;
 		system("pause");
 	}
-	
+
 }
 
+void ClearBoard()
+{
+
+}
+
+int main()
+{
+	PlayGame();
+	char board[9] = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+	std::cout << "Would you like to play again? (Select a number)" << std::endl;
+	int playAgain;
+	std::cout << "1. Yes        2. No" << std::endl;
+	std::cin >> playAgain;
+	while (playAgain == 1)
+	{
+		system("cls");
+		PlayGame();
+		std::cout << "Would you like to play again? (Select a number)" << std::endl;
+		std::cout << "1. Yes        2. No" << std::endl;
+		std::cin >> playAgain;
+	}
+
+}
