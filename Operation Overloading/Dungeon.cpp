455611c1c0b3c94f60a70d1bb7a97502fb09756f
bstelly <bstelly1999@gmail.com>
2017-09-30 23:57:20 -0500

    commit

--- Operation Overloading/Dungeon.cpp
+++ Operation Overloading/Dungeon.cpp
@@ -1,14 +1,38 @@
 #include "Dungeon.h"
 #include <iostream>
 
+
 Dungeon::Dungeon()
 {
-	int dungeon[10][10];
+	mRooms = Point2D(10, 10);
+	mPlayer;
+	mNumCols = 10;
+	mNumRows = 10;
+
+
+	
 }
 
 void Dungeon::GenRooms()
 {
-	
+	if (mNumCols == 10 && mNumRows == 10)
+	{
+		int dungeon[10][10];
+		for (int row = 0; row < 10; row++)
+		{
+			for (int col = 0; col < 10; col++)
+			{
+				std::cout << dungeon[row][col] << std::endl;
+			}
+		}
+	}
+
+	else
+	{
+		int row = mNumRows;
+		int col = mNumCols;
+		int dungeon[row][col];
+	}
 }
 
 Dungeon::Dungeon(Player* player, int rows, int cols)
--- Operation Overloading/Source.cpp
+++ Operation Overloading/Source.cpp
@@ -3,26 +3,26 @@
 #include "Player.h"
 int main()
 {
-	Point2D positionOne;									
-	Point2D positionTwo(10, 20);
-	Point2D newPositionOne = positionOne + positionTwo;		
-	Point2D newPositionTwo = positionOne - positionTwo;
-	Point2D newPositionThree = positionOne * positionTwo;
-	Point2D newPositionFour = positionTwo * 3;
-	std::cout << newPositionTwo.GetX() << std::endl;
-	std::cout << newPositionTwo.GetY() << std::endl;
-	newPositionFour.PrintPoint();
-	bool samePosition = newPositionThree == newPositionFour;
-	newPositionTwo += newPositionFour;
-	newPositionTwo -= newPositionFour;
-	std::cout << std::endl;
-	newPositionTwo.PrintPoint();
-	system("pause");
-	char direction;
-	Player brett("Brett");
-	std::cin >> direction;
-	brett.Move(direction);
-
+	//Point2D positionOne;									
+	//Point2D positionTwo(10, 20);
+	//Point2D newPositionOne = positionOne + positionTwo;		
+	//Point2D newPositionTwo = positionOne - positionTwo;
+	//Point2D newPositionThree = positionOne * positionTwo;
+	//Point2D newPositionFour = positionTwo * 3;
+	//std::cout << newPositionTwo.GetX() << std::endl;
+	//std::cout << newPositionTwo.GetY() << std::endl;
+	//newPositionFour.PrintPoint();
+	//bool samePosition = newPositionThree == newPositionFour;
+	//newPositionTwo += newPositionFour;
+	//newPositionTwo -= newPositionFour;
+	//std::cout << std::endl;
+	//newPositionTwo.PrintPoint();
+	//system("pause");
+	//char direction;
+	//Player brett("Brett");
+	//std::cin >> direction;
+	//brett.Move(direction);
+	
 
 
 	
