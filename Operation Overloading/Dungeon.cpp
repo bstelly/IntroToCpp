b4bc8d80b4fde3499a7e5a4aae9b95a97069bd89
bstelly <bstelly1999@gmail.com>
2017-10-01 22:53:29 -0500

    commit

--- Operation Overloading/Dungeon.cpp
+++ Operation Overloading/Dungeon.cpp
@@ -1,10 +1,10 @@
 #include "Dungeon.h"
 #include <iostream>
+#include <time.h>
 
 
 Dungeon::Dungeon()
 {
-	mRooms = Point2D(10, 10);
 	mPlayer;
 	mNumCols = 10;
 	mNumRows = 10;
@@ -15,23 +15,17 @@ Dungeon::Dungeon()
 
 void Dungeon::GenRooms()
 {
-	if (mNumCols == 10 && mNumRows == 10)
+	char player = 'X';
+	char emptySpace = ' ';
+	char pit = 'p';
+	char gold = 'g';
+	char wumpus = 'w';
+	for (int row = 0; row < mNumRows; row++)
 	{
-		int dungeon[10][10];
-		for (int row = 0; row < 10; row++)
+		for (int col = 0; col < mNumCols; col++)
 		{
-			for (int col = 0; col < 10; col++)
-			{
-				std::cout << dungeon[row][col] << std::endl;
-			}
+			
 		}
-	}
-
-	else
-	{
-		int row = mNumRows;
-		int col = mNumCols;
-		int dungeon[row][col];
 	}
 }
 
@@ -40,14 +34,42 @@ Dungeon::Dungeon(Player* player, int rows, int cols)
 	mNumRows = rows;
 	mNumCols = cols;
 	Player* player = player;
+	mRooms;
+	int dungeon[mNumRows][mNumCols];
+
 }
 
-bool Dungeon::CheckPlayerPosition()
+int Dungeon::CheckPlayerPosition()  // 1 == game being played, 2 == Game won, 0 == Game lost
 {
-	
+	if ('X' == 'p')
+	{
+		std::cout << "You have fallen into a pit. Game Over." << std::endl;
+		return 0;
+	}
+	if ('X' == 'w')
+	{
+		std::cout << "You have been caught by the wumpus. Game Over." << std::endl;
+		return 0;
+	}
+	if ('X' == ' ')
+	{
+		std::cout << "You move into a dark room." << std::endl;
+		return 1;
+	}
+	if ('X' == 'g')
+	{
+		std::cout << "You found the gold! YOU WIN!" << std::endl;
+		return 2;
+	}
 }
 
 void Dungeon::PrintRooms()
 {
-
+	for (int row = 0; row < mNumRows; row++)
+	{
+		for (int col = 0; col < mNumCols; col++)
+		{
+			std::cout << dungeon[mNumRows][mNumCols];
+		}
+	}
 }
